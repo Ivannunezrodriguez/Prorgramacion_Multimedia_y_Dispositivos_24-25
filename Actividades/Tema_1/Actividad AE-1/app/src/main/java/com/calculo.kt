0879171f2248bbf34507b37b2e1f9f5699b5062f
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2024-11-07 11:22:00 +0100

    1 subida

---
+++ Actividades/Tema_1/Actividad AE-1/app/src/main/java/com/calculo.kt
@@ -0,0 +1,25 @@
+package com
+
+class calculo {
+    val salarioBruto :double
+    val numPagas: int
+    var edad :int
+    var profesion: String
+    var gDiscapacidad : Double
+    var EstadoCivil: String
+    var numHijos :int
+
+    fun mostrarSalarioBruto{
+        println("El salario bruto es de $salarioBruto")
+    }
+
+    fun mostrarSalarioNeto{
+
+    }
+    fun retecionIRPF{
+
+    }
+    fun posibleDeducciones{
+
+    }
+}
